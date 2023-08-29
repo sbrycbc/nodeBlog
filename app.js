d822a7174e1ad5790acd0d6aec0b5f10a923590f
sbrycbc <sabriyecbc@gmail.com>
2023-08-29 21:15:38 +0200

    added html pages and fs module...

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const http = require('http')
+const fs = require('fs')
 
 const hostname = '127.0.0.1'
 const port = 3000
 
+const indexPage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+
+
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    /* console.log(req.url)
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello NODE.JS')
+    res.end('Hello NODE.JS') */
+
+    if (req.url === '/') {
+        return res.end(indexPage)
+    }else if (req.url === '/about'){
+        return res.end(aboutPage)
+    }else if (req.url === '/contact'){
+        return res.end(contactPage)
+    }else {
+        res.statusCode = 404
+        res.end('NOT FOUND')
+    }
 })
 
 server.listen(port, hostname, () => {
