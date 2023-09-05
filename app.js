2fceea58a4591558524029034f050bed478e1214
sbrycbc <sabriyecbc@gmail.com>
2023-09-05 16:12:23 +0200

    updated public and add sites with page and style..

--- app.js
+++ app.js
@@ -4,27 +4,19 @@ const app = express()
 const port = 3000
 const hostname = '127.0.0.1'
 
-app.use('/test', (req, res, next) => {
-    console.log('ich bin Middleware!!')
-    next()
-})
+
 
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'sites/index.html'))
 })
-
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'about.html'))
+    res.sendFile(path.resolve(__dirname, 'sites/about.html'))
 })
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'contact.html'))
+app.get('/blog', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'sites/blog.html'))
 })
-
-
 
 
 
@@ -37,40 +29,3 @@ app.listen(port, hostname, () => {
 })
 
 
-/* const http = require('http')
-const fs = require('fs')
-
-const hostname = '127.0.0.1'
-const port = 3000
-
-const indexPage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-
-
-const server = http.createServer((req, res) => {
-    /* console.log(req.url)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello NODE.JS') */
-
-    /* if (req.url === '/') {
-        return res.end(indexPage)
-    }else if (req.url === '/about'){
-        return res.end(aboutPage)
-    }else if (req.url === '/contact'){
-        return res.end(contactPage)
-    }else {
-        res.statusCode = 404
-        res.end('NOT FOUND')
-    }
-})
-
-server.listen(port, hostname, () => {
-    console.log(` Server läuft auf, http://${hostname}:${port}/`)
-}) */ 
-
-
-
-
-// mongodb+srv://sabriyecbc:hvqEncsFDWgAtOjP@sabriye.i22pwid.mongodb.net/
